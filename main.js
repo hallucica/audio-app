68c45ee13df31fd9bd31a733348071ed68923a99
Hallucica <91373998+hallucica@users.noreply.github.com>
2024-01-08 01:10:41 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,39 +1,41 @@
-console.log("hey");
+let audioElement = document.querySelector("audio[name='track']");
 
-const playButton = document.getElementById("play");
-
-
-const audioCtx = new AudioContext();
+let audioCtx;	// AudioContext
+let track;	// HTMLで記述した<audio>要素に対応するMediaElementAudioSourceNode
 
 document.addEventListener("DOMContentLoaded", () => {
-	// Audioクラスを生成する。これはHTMLの<audio>要素と同じと思っていい。
-	const audio = new Audio();
-	audio.volume = 0.3;
-	// audio.controls;			// 再生ボタンやシークバーといったコントロールを表示する
-	// audio.loop = true;		// 音声ファイルをループ再生する
-	// audio.currentTime = 0;	// 現在の再生時刻(秒)。読み込み前は以外の値にしない方がgood
+	if(!audioCtx) init();
 
 	// HTML で記述した<select>要素を取得
 	const selector = document.querySelector('select[name="audioList"]');
 
 	selector.addEventListener("change", (e) => {
 		// <audio>要素が再生状態の場合は一時停止する。
-		if(!audio.paused) audio.pause();
+		if(!audioElement.paused) audio.pause();
 
 		// <select>要素の value の値が空でなければ、その値を<audio>要素の src 属性に指定して再生
 		if(e.currentTarget.value !== "") {
-			audio.src = e.currentTarget.value;
-			console.log(`読み込むファイル：${audio.src}`);
-			console.log("再生");
+			// 自動再生ポリシーに従う
+			if(audioCtx.state === "suspended") audioCtx.resume();
+			audioElement.src = e.currentTarget.value;
+			audioElement.play();
 		}
 	});
 
 	window.addEventListener("beforeunload", (e) => {
-		if(!audio.paused) audio.pause();
+		if(!audioElement.paused) audio.pause();
 	});
 
-	let audioSource = audioCtx.createMediaElementSource(audio);
-	
-	audioSource.connect(audioCtx.destination);
-
 });
+
+// 初期化用
+init() {
+	// オーディオコンテキストの生成
+	audioCtx = new AudioContext();
+	// HTMLに記述した<audio>要素からMediaElementAudioSourceノードを生成
+	track = audioCtx.createMediaElementSource(audioElement);
+	// Gainノードを生成
+	const gainNode = audioCtx.createGain();
+	// ノードを接続
+	track.connect(gainNode).connect(audioCtx.destination);
+}
